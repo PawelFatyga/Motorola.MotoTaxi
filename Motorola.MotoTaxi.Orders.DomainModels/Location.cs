d55e233b5fb200dfb7b0c5c14ed241425705bd6a
Student <Student@DESKTOP-3C6VS0P>
2019-03-25 14:10:16 +0100

    Create order model and WebApi

---
+++ Motorola.MotoTaxi.Orders.DomainModels/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace Motorola.MotoTaxi.Orders.DomainModels
+{
+    public struct Location
+    {
+        public double Latitude { get; set; }
+        public int Longitude { get; set; }
+    }
+}
---
+++ Motorola.MotoTaxi.Orders.IServices/IEntityService.cs
@@ -0,0 +1,9 @@
+﻿namespace Motorola.MotoTaxi.Orders.IServices
+{
+    public interface IEntityService<TEntity>
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entyity);
+        TEntity Get(int id);
+    }
+}
