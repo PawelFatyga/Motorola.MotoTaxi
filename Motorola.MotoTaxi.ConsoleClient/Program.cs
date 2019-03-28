afc58c9c601244610743c8761dc6da870bf43071
Student <Student@DESKTOP-3C6VS0P>
2019-03-28 11:01:28 +0100

    Add Location Api

---
+++ Motorola.MotoTaxi.ConsoleClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Motorola.MotoTaxi.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Task.Run(() => GetOrdersTest());
+
+            Console.ReadKey();
+        }
+
+        private static async Task GetOrdersTest()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5000");
+                var response = await client.GetAsync("api/orders");
+
+                if(response.IsSuccessStatusCode)
+                {
+                    
+
+                }
+            }
+        }
+    }
+}
